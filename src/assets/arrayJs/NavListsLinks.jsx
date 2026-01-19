6e770e1f5b28e1be724ff831d5c8dcd9cadff92c
marcotristani <marco.tristani@hotmail.it>
2026-01-19 19:14:09 +0100

    flat: dinamicizzata navListLink footer con array di oggetti esterni importato da assets

---
+++ src/assets/arrayJs/NavListsLinks.jsx
@@ -0,0 +1,196 @@
+const NavListsLinks = [
+  {
+    id: 1,
+    column: 1,
+    links: [
+      {
+        id: 1,
+        src: "#",
+        title: true,
+        name: "DC COMICS",
+      },
+      {
+        id: 2,
+        src: "#",
+        title: false,
+        name: "Characters",
+      },
+      {
+        id: 3,
+        src: "#",
+        title: false,
+        name: "Comics",
+      },
+      {
+        id: 4,
+        src: "#",
+        title: false,
+        name: "Movies",
+      },
+      {
+        id: 5,
+        src: "#",
+        title: false,
+        name: "TV",
+      },
+      {
+        id: 6,
+        src: "#",
+        title: false,
+        name: "Games",
+      },
+      {
+        id: 7,
+        src: "#",
+        title: false,
+        name: "Videos",
+      },
+      {
+        id: 8,
+        src: "#",
+        title: false,
+        name: "News",
+      },
+      {
+        id: 9,
+        src: "#",
+        title: true,
+        name: "SHOP",
+      },
+      {
+        id: 10,
+        src: "#",
+        title: false,
+        name: "Shop DC",
+      },
+      {
+        id: 11,
+        src: "#",
+        title: false,
+        name: "Shop DC Collectibles",
+      },
+    ],
+  },
+  {
+    id: 2,
+    column: 2,
+    links: [
+      {
+        id: 1,
+        src: "#",
+        title: true,
+        name: "DC",
+      },
+      {
+        id: 2,
+        src: "#",
+        title: false,
+        name: "Terms of Use",
+      },
+      {
+        id: 3,
+        src: "#",
+        title: false,
+        name: "Privacy policy(New)",
+      },
+      {
+        id: 4,
+        src: "#",
+        title: false,
+        name: "Ad Choices",
+      },
+      {
+        id: 5,
+        src: "#",
+        title: false,
+        name: "Advertising",
+      },
+      {
+        id: 6,
+        src: "#",
+        title: false,
+        name: "Jobs",
+      },
+      {
+        id: 7,
+        src: "#",
+        title: false,
+        name: "Subscriptions",
+      },
+      {
+        id: 8,
+        src: "#",
+        title: false,
+        name: "Talent Workshops",
+      },
+      {
+        id: 9,
+        src: "#",
+        title: false,
+        name: "CPSC Certificates",
+      },
+      {
+        id: 10,
+        src: "#",
+        title: false,
+        name: "Ratings",
+      },
+      {
+        id: 11,
+        src: "#",
+        title: false,
+        name: "Shop Help",
+      },
+      {
+        id: 11,
+        src: "#",
+        title: false,
+        name: "Contact Us",
+      },
+    ],
+  },
+  {
+    id: 3,
+    column: 3,
+    links: [
+      {
+        id: 1,
+        src: "#",
+        title: true,
+        name: "SITIES",
+      },
+      {
+        id: 2,
+        src: "#",
+        title: false,
+        name: "DC",
+      },
+      {
+        id: 3,
+        src: "#",
+        title: false,
+        name: "MAD Magazine",
+      },
+      {
+        id: 4,
+        src: "#",
+        title: false,
+        name: "DC Kids",
+      },
+      {
+        id: 5,
+        src: "#",
+        title: false,
+        name: "DC Universe",
+      },
+      {
+        id: 6,
+        src: "#",
+        title: false,
+        name: "DC Power Visa",
+      },
+    ],
+  },
+];
+
+export default NavListsLinks;
--- src/components/FooterSection/NavList.jsx
+++ src/components/FooterSection/NavList.jsx
@@ -1,96 +1,14 @@
+import NavListsLinks from "../../assets/arrayJs/NavListsLinks";
+
 const NavList = () => {
   return (
     <div className="container-nav-list padding-x">
       <nav>
-        <div className="col">
-          <h1>DC COMICS</h1>
-          <ul>
-            <li>
-              <a href="#">Characters</a>
-            </li>
-            <li>
-              <a href="#">Comics</a>
-            </li>
-            <li>
-              <a href="#">Movies</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">Games</a>
-            </li>
-            <li>
-              <a href="#">Videos</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-          </ul>
-          <h1>SHOP</h1>
-          <ul>
-            <li>
-              <a href="">Shop DC</a>
-            </li>
-            <li>
-              <a href="">Shop DC Collectibles</a>
-            </li>
-          </ul>
-        </div>
-        <div className="col">
-          <h1>DC</h1>
-          <li>
-            <a href="">Terms Of Use</a>
-          </li>
-          <li>
-            <a href="">Privacy policy(New)</a>
-          </li>
-          <li>
-            <a href="">Ad Choices</a>
-          </li>
-          <li>
-            <a href="">Advertising</a>
-          </li>
-          <li>
-            <a href="">Jobs</a>
-          </li>
-          <li>
-            <a href="">Subscriptions</a>
-          </li>
-          <li>
-            <a href="">Talent Workshops</a>
-          </li>
-          <li>
-            <a href="">CPSC Certificates</a>
-          </li>
-          <li>
-            <a href="">Ratings</a>
-          </li>
-          <li>
-            <a href="">Shop Help</a>
-          </li>
-          <li>
-            <a href="">Contact Us</a>
-          </li>
-        </div>
-        <div className="col">
-          <h1>SITES</h1>
-          <li>
-            <a href="">DC</a>
-          </li>
-          <li>
-            <a href="">MAD Magazine</a>
-          </li>
-          <li>
-            <a href="">DC Kids</a>
-          </li>
-          <li>
-            <a href="">DC Universe</a>
-          </li>
-          <li>
-            <a href="">DC Power Visa</a>
-          </li>
-        </div>
+        {NavListsLinks.map((listLinks) => (
+          <div className="col" key={listLinks.id}>
+            <ul>{GenerateList(listLinks.links)}</ul>
+          </div>
+        ))}
       </nav>
       <figure>
         <img src="src/assets/img/dc-logo-bg.png" alt="logo dc" />
@@ -98,5 +16,13 @@ const NavList = () => {
     </div>
   );
 };
+
+function GenerateList(list) {
+  return list.map((link) => (
+    <li key={link.id} className={link.title ? "title" : ""}>
+      <a href={link.src}>{link.name}</a>
+    </li>
+  ));
+}
 
 export default NavList;
