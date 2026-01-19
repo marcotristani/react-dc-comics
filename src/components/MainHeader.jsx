96f18689de43f05a2c57e874b70f636019559904
marcotristani <marco.tristani@hotmail.it>
2026-01-19 16:06:58 +0100

    flat: dinamicizzato header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,3 +1,66 @@
+const navLinks = [
+  {
+    id: 1,
+    name: "Characters",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 2,
+    name: "Comics",
+    src: "#",
+    selected: true,
+  },
+  {
+    id: 3,
+    name: "Movies",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 4,
+    name: "TV",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 5,
+    name: "Games",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 6,
+    name: "Collectibles",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 7,
+    name: "Videos",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 8,
+    name: "Fans",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 9,
+    name: "News",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 10,
+    name: "Shop",
+    src: "#",
+    selected: false,
+  },
+];
+
 const MainHeader = () => {
   return (
     <header className="padding-x">
@@ -7,18 +70,19 @@ const MainHeader = () => {
         </a>
       </figure>
       <nav>
-        <a href="">CHARACTERS</a>
-        <a href="" className="nav-selected">
-          COMICS
-        </a>
-        <a href="">MOVIES</a>
-        <a href="">TV</a>
-        <a href="">GAMES</a>
-        <a href="">COLLECTIBLES</a>
-        <a href="">VIDEOS</a>
-        <a href="">FANS</a>
-        <a href="">NEWS</a>
-        <a href="">SHOP</a>
+        <ul>
+          {navLinks.map((link) => (
+            <li>
+              <a
+                href={link.src}
+                key={link.id}
+                className={link.selected ? "nav-selected" : ""}
+              >
+                {link.name.toUpperCase()}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
