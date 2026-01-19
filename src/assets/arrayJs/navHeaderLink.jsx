13c70528751c203bb84648641590da081e45effb
marcotristani <marco.tristani@hotmail.it>
2026-01-19 18:12:09 +0100

    flat: importati array da documenti esterni costruiti in assets

---
+++ src/assets/arrayJs/navHeaderLink.jsx
@@ -0,0 +1,63 @@
+const navLinks = [
+  {
+    id: 1,
+    name: "Characters",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 2,
+    name: "Comics",
+    src: "#",
+    selected: true,
+  },
+  {
+    id: 3,
+    name: "Movies",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 4,
+    name: "TV",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 5,
+    name: "Games",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 6,
+    name: "Collectibles",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 7,
+    name: "Videos",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 8,
+    name: "Fans",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 9,
+    name: "News",
+    src: "#",
+    selected: false,
+  },
+  {
+    id: 10,
+    name: "Shop",
+    src: "#",
+    selected: false,
+  },
+];
+export default navLinks;
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,72 +1,12 @@
-const navLinks = [
-  {
-    id: 1,
-    name: "Characters",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 2,
-    name: "Comics",
-    src: "#",
-    selected: true,
-  },
-  {
-    id: 3,
-    name: "Movies",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 4,
-    name: "TV",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 5,
-    name: "Games",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 6,
-    name: "Collectibles",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 7,
-    name: "Videos",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 8,
-    name: "Fans",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 9,
-    name: "News",
-    src: "#",
-    selected: false,
-  },
-  {
-    id: 10,
-    name: "Shop",
-    src: "#",
-    selected: false,
-  },
-];
+import navLinks from "../assets/arrayJs/navHeaderLink";
+import logo from "../assets/img/dc-logo.png";
 
 const MainHeader = () => {
   return (
     <header className="padding-x">
       <figure>
         <a href="">
-          <img src="src/assets/img/dc-logo.png" alt="logo dc" />
+          <img src={logo} alt="logo dc" />
         </a>
       </figure>
       <nav>
